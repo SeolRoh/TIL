d77be3eaac957c411f5b19b725cd1bb2d0b84432
SeolRoh <seven800@naver.com>
2020-04-05 16:33:03 +0900

    20200405_Java_scope_복습

--- Java/복습/ScopeDemo.java
+++ Java/복습/ScopeDemo.java
@@ -1,6 +1,9 @@
 public class ScopeDemo {
     static void a() {
         int i =0;
+        // static int i = 0;
+        // 클래스 변수로 선언하면 무한 반복이 일어난다.
+        // 클래스 변수는 모든 메소드에서 접근이 가능하기 때문이다.
     }
 
     public static void main(String[] args) {
