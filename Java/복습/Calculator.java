8a4eeb78e24c9261228b33e6bfa276f9d4b65796
SeolRoh <seven800@naver.com>
2020-04-19 23:52:40 +0900

    20200419_상속공부_Update

--- Java/복습/Calculator.java
+++ Java/복습/Calculator.java
@@ -1,8 +1,13 @@
+class substractionableCalculator extends Calculator{
+        int substract(){
+        return left - right;
+    }
+}
 public class Calculator {
     int left;
     int right;
 
-    void method (int left, int right){
+    void setOperands (int left, int right){
         this.left = left;
         this.right = right;
     }
@@ -10,13 +15,21 @@ public class Calculator {
         return left + right ;
     }
 
+
     public static void main(String[] args) {
         int result;
 
         Calculator a1 = new Calculator();
-        a1.method(10,20);
+        a1.setOperands(10,20);
         result = a1.add();
 
+        System.out.println(result);
+
+        substractionableCalculator a1 = new substractionableCalculator();
+        a1.setOperands(30,40);
+        result = a1.add();
+        System.out.println(result);
+        result = a1.substract();
         System.out.println(result);
     }
 }
